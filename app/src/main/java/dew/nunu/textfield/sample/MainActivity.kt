4663061e6ac69007e801314bc657561b062238e2
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-07 10:44:14 +0900

    :label: Add navigation types

--- app/src/main/java/dew/nunu/textfield/sample/MainActivity.kt
+++ app/src/main/java/dew/nunu/textfield/sample/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dew.nunu.textfield.sample.ui.theme.TextFieldSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             TextFieldSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") {
+                                // HomeScreen()
+                            }
+                            composable("text-field") {
+                                // TextFieldScreen()
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TextFieldSampleTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/dew/nunu/textfield/sample/navigation/Type.kt
@@ -0,0 +1,15 @@
+package dew.nunu.textfield.sample.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Home
+
+@Serializable
+data object Filter
+
+@Serializable
+data object Identifier
+
+@Serializable
+data object Otp
