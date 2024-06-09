446823bf19d800d8effa2359f3c9c47ddc2da239
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-06-09 14:41:31 +0900

    :heavy_plus_sign: Add dependencies
    - Compose Bom Alpha(by Chris Banes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(platform(libs.androidx.compose.bom.alpha))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
